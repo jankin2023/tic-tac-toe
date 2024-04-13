289b4d4a74d116afe21cbb324b7ebc555e0870ce
刘君宝 <junbaoliu2023@163.com>
2024-04-13 15:27:12 +0800

    修改vite配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+    if (mode === "production") {
+        return {
+            base: "/tic-tac-toe/",
+        };
+    }
+
+    return {
+        plugins: [react()],
+    };
+});
